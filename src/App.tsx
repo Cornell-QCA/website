b32b39d34f2a5ec49763fd9e2c07c977b5d794b8
Haadi Khan <haadikhan05@gmail.com>
2024-08-21 01:59:27 -0400

    Fixed papaparse dep issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import Card from './components/people/card'
 import People from './components/people/page'
 import Page from './components/home/page'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='flex flex-col items-center justify-center py-2'>
       <Page />
