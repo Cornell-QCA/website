a123ddfc0c77eb1fb7b57e95e2ab939f2787b28a
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2024-08-27 19:21:18 -0400

    save

--- src/components/links/page.tsx
+++ src/components/links/page.tsx
@@ -10,11 +10,16 @@ const Links: React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center">
         
         <Link
-          title = {'Hi'}
-          link = {'http://www.google.com'}
-          desc = {'SOmething'}
+          title = {'QCA Feedback Form'}
+          link = {'https://docs.google.com/forms/d/e/1FAIpQLSdeWrH2-RdOJu3I_snCvQfZeDlJ9i-R3bJKfbC9JIYJAjFEAw/viewform'}
+          desc = {'Any advice is greatly appreciated!'}
           ></Link>
-
+        
+        <Link
+          title = {'Funamentals Syllabus'}
+          link = {'https://docs.google.com/document/d/1wZHFWIt5Mhtjy4JH2ew3t48ZjnR-gXfNrvEopHNgT4E/edit#heading=h.xol6henfliy8'}
+          desc = {''}
+          ></Link>
       </div>
     </div>
   );
