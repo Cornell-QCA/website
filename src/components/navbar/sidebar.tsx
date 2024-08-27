324da6776a7d26454c99400d70578fa65d190e6b
Haadi Khan <haadikhan05@gmail.com>
2024-08-27 07:53:43 -0400

    Fixed sidebar routing w/ HashLink

--- src/components/navbar/sidebar.tsx
+++ src/components/navbar/sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 interface SidebarProps {
   isOpen: boolean;
@@ -8,7 +9,6 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   return (
     <>
-      {/* Overlay */}
       {isOpen && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 z-30"
@@ -16,31 +16,28 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
         ></div>
       )}
 
-      {/* Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-gray-100 transform transition-transform duration-300 ${
-          isOpen ? 'translate-x-0' : 'translate-x-full'
-        } z-40`}
+        className={`fixed top-0 right-0 h-full w-64 bg-gray-100 transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'
+          } z-40`}
       >
         <nav className="mt-16 space-y-4">
-          <a href="/home" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
-            Home
-          </a>
-          <a href="/blurb" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
+          <HashLink to="/#home" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
+            Home</HashLink>
+          <HashLink to="/#blurb" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
             About
-          </a>
-          <a href="/apply" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
+          </HashLink>
+          <HashLink to="/#apply" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
             Apply
-          </a>
-          <a href="/projects" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
+          </HashLink>
+          <HashLink to="/#projects" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
             Projects
-          </a>
-          <a href="/people" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
+          </HashLink>
+          <HashLink to="/#people" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
             People
-          </a>
-          <a href="/events" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
+          </HashLink>
+          <HashLink to="/#events" className="block px-6 py-2 hover:bg-gray-200" onClick={onClose}>
             Events
-          </a>
+          </HashLink>
         </nav>
       </div>
     </>
